26c3ae9bdee3f1d8d334f74a31b85167182b1e42
devxoul <devxoul@gmail.com>
2012-09-14 14:53:05 +0900

    Initial Commit

---
+++ Dish by.me/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Dish by.me
+//
+//  Created by 전 수열 on 12. 9. 14..
+//  Copyright (c) 2012년 Joyfl. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+	@autoreleasepool {
+	    return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+	}
+}
