c58c55ac485e5b6cfc8e5325e477cad4352b5e64
전수열 <devxoul@gmail.com>
2013-03-07 02:13:06 +0900

    Unicode out of range error was fixed.

--- Dish by.me/JLHangulUtils.m
+++ Dish by.me/JLHangulUtils.m
@@ -18,8 +18,13 @@
 	unichar jungsungIndex = ( ( unicode - jongsungIndex ) / 28 ) % 21;
 	unichar chosungIndex = ( unicode - jongsungIndex - 28 * jungsungIndex ) / 588;
 	
+	if( chosungIndex < 0 || chosungIndex >= JLHangulChosungs.count ) return nil;
 	NSString *chosung = [JLHangulChosungs objectAtIndex:chosungIndex];
+	
+	if( jungsungIndex < 0 || jungsungIndex >= JLHangulJungsungs.count ) return @[chosung, @"", @""];
 	NSString *jungsung = [JLHangulJungsungs objectAtIndex:jungsungIndex];
+	
+	if( jongsungIndex < 0 || jongsungIndex >= JLHangulJongsungs.count ) return @[chosung, jungsung, @""];
 	NSString *jongsung = [JLHangulJongsungs objectAtIndex:jongsungIndex];
 	
 	return @[chosung, jungsung, jongsung];
