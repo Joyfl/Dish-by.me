7a8aa8f36dc26d84d3774daa0ed5366976be3109
전수열 <devxoul@gmail.com>
2013-03-22 03:00:07 +0900

    Updated license of UIButton+TouchAreaInsets.

--- Dish by.me/Categories/UIButton+TouchAreaInsets.m
+++ Dish by.me/Categories/UIButton+TouchAreaInsets.m
@@ -1,10 +1,21 @@
-//
-//  UIButton+TouchAreaInsets.m
-//  Dish by.me
-//
-//  Created by 전수열 on 13. 3. 22..
-//  Copyright (c) 2013년 Joyfl. All rights reserved.
-//
+/*
+ * UIButton+TouchAreaInsets.m
+ *
+ *            DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE
+ *                    Version 2, December 2004
+ *
+ * Copyright (C) 2013 Joyfl
+ *
+ * Everyone is permitted to copy and distribute verbatim or modified
+ * copies of this license document, and changing it is allowed as long
+ * as the name is changed.
+ *
+ *            DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE
+ *   TERMS AND CONDITIONS FOR COPYING, DISTRIBUTION AND MODIFICATION
+ *
+ *  0. You just DO WHAT THE FUCK YOU WANT TO.
+ *
+ */
 
 #import "UIButton+TouchAreaInsets.h"
 #import <objc/runtime.h>
