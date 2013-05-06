aee9709aee85c63b914e46735907afaca42d2f9a
전수열 <devxoul@gmail.com>
2013-05-06 23:57:40 +0900

    Call delegate method after uploading dish.

--- Dish by.me/ViewControllers/WritingViewController.m
+++ Dish by.me/ViewControllers/WritingViewController.m
@@ -686,6 +686,7 @@ enum {
 				double delayInSeconds = 1.0;
 				dispatch_time_t popTime = dispatch_time(DISPATCH_TIME_NOW, (int64_t)(delayInSeconds * NSEC_PER_SEC));
 				dispatch_after(popTime, dispatch_get_main_queue(), ^(void){
+					[self.delegate writingViewControllerDidFinishUpload:self];
 					[self dismissViewControllerAnimated:YES completion:nil];
 				});
 			} );
